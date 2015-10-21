2296cd405a598173075c915aafdaaf5fd0b18d9a
Ryan B Hayward <hayward@ualberta.ca>
2015-10-21 17:39:35 -0600

    sssp now ssspDemo

--- paths/sssp.py
+++ paths/sssp.py
@@ -1,4 +1,4 @@
-# single source shortest path 
+# single source shortest path  (source means start)
 # for graphs/digraphs with non-negative edge weights
 # (simple implementation: no priority queue)
 def weightSum(G):
@@ -8,42 +8,44 @@ def weightSum(G):
   return sum
 
 def showAll(G,D,P):
-  print '    ',
+  print '\nprnt',
+  for v in sorted(G): print P[v],
+  print '\nnode',
   for v in sorted(G): print v,
   print '\ndist',
   for v in sorted(G): print D[v],
-  print '\nprnt',
-  for v in sorted(G): print P[v],
   print ''
 
-def show(G,F,D):
-  for v in sorted(F): print v,D[v],' ',
-  print ''
+def showFringe(G,F,D,inf):
+  print '  fringe',
+  for v in sorted(F): 
+    if D[v]!=inf: 
+      print v,D[v],' ',
 
-def sssp(G,source):
+def sssp(G,start):
   infinity = 999 + weightSum(G)
   dist, parent, fringe = {}, {}, []
   for v in G:
     dist[v], parent[v] = infinity, -1
     fringe.append(v)
-  dist[source],parent[source] = 0, source
+  dist[start],parent[start] = 0, start
 
   doneEarly = False
-  print '\nsource node:',source,'\n'
   while len(fringe)>0 and not doneEarly:
     ndxMin = 0
     for j in range(1,len(fringe)):
       if dist[fringe[j]] < dist[fringe[ndxMin]]: ndxMin = j
     u = fringe[ndxMin]
-    show(G,fringe,dist)
-    print 'final distance:',u,dist[u],'\n'
+    showFringe(G,fringe,dist,infinity)
+    print '\npick',u,':',
     fringe.pop(ndxMin)
     if (dist[u] == infinity):  doneEarly = True
     for (v,duv) in G[u]:
       if dist[v] > dist[u] + duv: 
         dist[v] = dist[u] + duv
         parent[v] = u
-        print '  ',v,dist[v],'via',u
+        print v,dist[v],' ',
+    print ''
   showAll(G,dist,parent)
 
 G = {'A': [['B',4],['C',2]],
@@ -58,4 +60,5 @@ G2 = {'A': [['B',8],['C',8],['D',9],['E',13],['F',4],['G',11]],
      'E': [['A',13],['B',12],['C',7],['D',1],['F',18],['G', 6]],
      'F': [['A',4],['B',1],['C',15],['D',13],['F',18],['G', 5]],
      'G': [['A',11],['B',18],['C',6],['D',9],['F',6],['G', 5]]}
+G3 = {'A': []}
 sssp(G,'A')
