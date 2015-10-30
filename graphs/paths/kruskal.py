fc8a5c5ccfef9dc263dd8d5322644dc2c3ab55bd
Ryan B Hayward <hayward@ualberta.ca>
2015-10-29 18:12:49 -0600

    kruskal

---
+++ graphs/paths/kruskal.py
@@ -0,0 +1,44 @@
+# simple Kruskal mst
+import weighted 
+
+# convert from weighted adjacency list 
+#         to tuple list:   (v,w,wt_vw)
+def createEdgeList(G):
+  L = []
+  for v in G:
+    for (w,wt) in G[v]:
+      if ord(v)<ord(w): L.append((v,w,wt))
+  return L
+
+# extract tuple with min wt
+def extractmin(L):
+  ndxMin, valMin = 0, L[0][2]
+  for j in range(1,len(L)):
+    t = L[j]
+    if t[2] < valMin:
+      ndxMin, valMin = j, t[2]
+  return L.pop(ndxMin)
+
+# root of component containing v
+def root(v,parent):
+  while parent[v] != v:
+    v = parent[v]
+  return v
+
+def kruskalDemo(G):
+  L = createEdgeList(G)
+  parent = {}
+  for v in G: parent[v] = v
+
+  while len(L) > 0:
+    t = extractmin(L)
+    a, b = t[0], t[1]
+    ra, rb = root(a,parent), root(b,parent)
+    print a, ra, b, rb,
+    if ra != rb:
+      print 'add edge',a,b,t[2]
+      parent[ra] = rb
+    else:
+      print 'reject  ',a,b
+
+kruskalDemo(weighted.G4)
