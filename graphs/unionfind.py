dc18a40e94605acb19eeda946f6d41148bc8043e
Ryan B Hayward <hayward@ualberta.ca>
2015-11-09 16:18:41 -0700

    union by rank

--- graphs/unionfind.py
+++ graphs/unionfind.py
@@ -1,16 +1,34 @@
-def myfind(x, parents):
+# use simple find,      simple union, or
+#     gp compress find, simple union, or
+#     simple find,      union by rank 
+
+def UFfind(v,P): # simple find
+  while P[v] != v:
+    v = P[v]
+  return v
+
+def UFfindGP(x, parents): # find, gp compress
   px = parents[x]
   if x==px: return x
-  gx = parents[px]
+  gx = parents[px] # grandparent
   while px != gx:
     parents[x] = gx
     x = px ; px = gx ; gx = parents[gx]
   return px
 
-def myunion(x,y,parents): 
-  rx = myfind(x,parents)
-  ry = myfind(y,parents)
-  parents[rx] = ry
+def UFunion(x,y,P): # simple union
+  rootx, rooty = UFfind(x,P), UFfind(y,P)
+  P[rootx] = rooty
+
+def UFunionBR(v,w,P,R): # union by rank
+  rv,rw = find(v),find(w)
+  if   R[rv] < R[rw]: 
+    P[rv] = rw
+  elif R[rv] > R[rw]: 
+    P[rw] = rv
+  else:
+    P[rv] = rw
+    R[rw] += 1
 
 n = 16
 P = []
@@ -22,5 +40,5 @@ t = 2
 while t <= n:
   print t
   for j in range(n/t):
-    myunion(t*j, t*j+t/2,P) ; print P
+    UFunion(t*j, t*j+t/2,P) ; print P
   t *= 2
