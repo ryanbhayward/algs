183869068b2ec68450db6d6386f6ee06922eeb6e
Ryan B Hayward <hayward@ualberta.ca>
2015-11-10 13:53:29 -0700

    brute force knapsack, plus edits

--- dynamic/knap.py
+++ dynamic/knap.py
@@ -1,7 +1,10 @@
-import random, operator
-def genvector(n):  
+from operator  import mul
+from random    import randint
+from itertools import chain, combinations
+
+def genvector(n):
   v = [] 
-  for j in range(n): v.append(random.randint(n,2*n))
+  for j in range(n): v.append(randint(n,2*n))
   return v
 
 def showRows(a,b,K): # last b-a rows
@@ -21,7 +24,25 @@ def sack(n,W,K):  # given W, find 0/1 solution vector
       w -= wt[ndx]
   return inSolution
 
-#python indexing:  val[t],wt[t] corresponds to K[t+1]
+def powerset(iterable):
+#https://docs.python.org/2/library/itertools.html#recipes
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def bruteforce(n,val,wt,W):
+  indices = range(n) # [0 1 .. n-1]
+  bestVal = 0
+  for indexset in powerset(indices):
+    v, w= 0, 0
+    for t in indexset: 
+      v+= val[t]
+      w+= wt[t]
+    if w <= W and v > bestVal: 
+      bestVal = v
+      print indexset, w, v
+
+#python indexing: val[t],wt[t] corresponds to K[][t+1]
 def solveknapsack(val,wt,W): 
   n = len(val)
   K = [[0 for j in xrange(n+1)] for w in xrange(W+1)]
@@ -29,14 +50,16 @@ def solveknapsack(val,wt,W):
     for w in range(W+1):
       if wt[j-1]>w: K[w][j] = K[w][j-1]
       else: K[w][j] = max(K[w][j-1], K[w-wt[j-1]][j-1]+val[j-1])
-  lastfew = 25   
-  showRows(W+1-lastfew,W+1,K) # print last few rows of K
+  lastfew = 30
+  showRows(max(W+1-lastfew,0),W+1,K) # print last few rows of K
   solvec = sack(n,W,K)
-  print '\n', sum(map(operator.mul, solvec, val)), solvec
+  print '\n', sum(map(mul, solvec, wt)), solvec, sum(map(mul, solvec, val))
     
-n = 200
-W, val, wt = (n*n*3)/4, genvector(n), genvector(n)
+#n = 25
+#W, val, wt = (n*n*3)/4, genvector(n), genvector(n)
+n,W,val,wt =  6, 27, [6, 9, 7, 9, 8, 7], [11, 6, 8, 10, 8, 9]
 print 'val  ', ; print(val)
 print 'wt   ', ; print(wt)
 print 'max value knapsack with weight at most', W, '?'
 solveknapsack(val,wt,W)
+#bruteforce(n,val,wt,W)
