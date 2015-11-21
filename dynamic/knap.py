6127089f74abd4c3243cb777c89f3c61c00992d4
Ryan B Hayward <hayward@ualberta.ca>
2015-11-21 14:27:55 -0700

    added knap DP by value

--- dynamic/knap.py
+++ dynamic/knap.py
@@ -7,10 +7,13 @@ def genvector(n):
   for j in range(n): v.append(randint(n,2*n))
   return v
 
-def showRows(a,b,K): # last b-a rows
-  print "..."
-  for w in range(a,b):
-    print w, ; print K[w]
+def showRows(a,b,K,inf): # rows a .. b-1
+  for w in range(a,b): #inf printed as '-'
+    print '%3d'%w,
+    for k in K[w]:
+      if k==inf: print '  -',
+      else: print '%3d'%k,
+    print ''
 
 def sack(n,W,K):  # given W, find 0/1 solution vector
   inSolution = [0 for j in xrange(n)]
@@ -30,20 +33,22 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-def bruteforce(n,val,wt,W):
-  indices = range(n) # [0 1 .. n-1]
+def knapBF(val,wt,W): # brute force knapsack
+  indices = range(len(val)) # [0 1 .. n-1]
   bestVal = 0
+  print 'subset    wt val   max wt',W
   for indexset in powerset(indices):
-    v, w= 0, 0
+    v, w = 0, 0
     for t in indexset: 
       v+= val[t]
       w+= wt[t]
     if w <= W and v > bestVal: 
       bestVal = v
-      print indexset, w, v
+      print indexset, w,v
 
-#python indexing: val[t],wt[t] corresponds to K[][t+1]
-def solveknapsack(val,wt,W): 
+def knapDP(val,wt,W): #usual dyn. prog. knap, by weight
+#warning: need an extra 0th column, so shift indexing:
+#val[t],wt[t] corresponds to K[][t+1]
   n = len(val)
   K = [[0 for j in xrange(n+1)] for w in xrange(W+1)]
   for j in range(1,n+1):
@@ -51,16 +56,29 @@ def solveknapsack(val,wt,W):
       if wt[j-1]>w: K[w][j] = K[w][j-1]
       else: K[w][j] = max(K[w][j-1], K[w-wt[j-1]][j-1]+val[j-1])
   lastfew = 30
-  showRows(max(W+1-lastfew,0),W+1,K) # print last few rows of K
+  showRows(max(W+1-lastfew,0),W+1,K,1+sum(wt)) # print last few rows of K
   solvec = sack(n,W,K)
-  print '\n', sum(map(mul, solvec, wt)), solvec, sum(map(mul, solvec, val))
+  print '\n', sum(map(mul, solvec, wt)), solvec, sum(map(mul, solvec, val)),'\n'
     
-n = 5
+def knapDPV(val,wt,V): #dynamic programming by value
+  # A[v][j] is min weight of subset of items 0..j with exact val v
+  infinity = 1 + sum(val) #larger than any possible sum of values
+  n = len(val)
+  A = [[infinity for j in xrange(n)] for v in xrange(V+1)]
+  for j in range(n): A[0][j] = 0
+  A[val[0]][0] = wt[0]  # end initialization
+  for v in range(1,V+1):  # row    0 already initialized
+    for j in range(1,n):  # column 0 "       "
+      new, subv = A[v][j-1], v-val[j]
+      if subv >= 0: new = min(new, A[subv][j-1] + wt[j])
+      A[v][j] = new
+  showRows(0,len(A),A,infinity)
+
+n = 8
 W, val, wt = (n*n*3)/4, genvector(n), genvector(n)
 #n,W,val,wt =  6, 27, [6, 9, 7, 9, 8, 7], [11, 6, 8, 10, 8, 9]
-n,W,val,wt = 5, 23, [7, 6, 10, 6, 9], [5, 8, 10, 9, 6]
-print 'val  ', ; print(val)
-print 'wt   ', ; print(wt)
-print 'max value knapsack with weight at most', W, '?'
-solveknapsack(val,wt,W)
-#bruteforce(n,val,wt,W)
+#n,W,val,wt = 5, 23, [7, 6, 10, 6, 9], [5, 8, 10, 8, 5]
+knapBF(val,wt,W)
+knapDP(val,wt,W)
+knapDPV(val,wt,sum(val))
+print 'val', val; print 'wt ', wt
