7eb3d63fe92a667d84c0294b22fe921990d0e745
Ryan Hayward <hayward@therien.cs.ualberta.ca>
2017-03-31 14:01:49 -0600

    more intuitive output

--- numeric/millerRabin.py
+++ numeric/millerRabin.py
@@ -19,7 +19,8 @@ def compositeWitness(w,n,verbose): # miller rabin
     return True    # yes, composite
   return False       # no, probably prime
 
-def isComposite(n,t,verbose): # t tries to show composite
+def isComposite(n,t,verbose): # t trials to show composite
+  print '  seek composite witness:',
   if verbose: print n,
   knownComposite = False
   tries = 0
@@ -45,13 +46,14 @@ def primegen(n,t,verbose): # find n-bit probable-prime
     a = 2*random.randint(low,high)+1
     if verbose: print a
     found = not isComposite(a,t,verbose)
-  if verbose: print attempts, 'attempts'
+  if verbose: print '\n', attempts, 'attempts before probable-prime found'
   return a, attempts  # Prob(a prime) >= 1 - (1/4)^t
 
-primetest(987613,1,True)
+#primetest(987613,1,True)
 
-numbits, experiments, sum = 100, 100, 0
+numbits, experiments, t, sum = 100, 10, 20, 0
 for j in range(experiments):
-  sum += primegen(numbits,20,False)[1]
-print 'avg trials to find prime', sum/(experiments*1.0)
-print 'expected number of trials', math.log(pow(2,numbits))/2.0
+  print 'experiment',j, ': find probable-prime, prob. >= 1.0 -', pow(.25,t), '\n'
+  sum += primegen(numbits,t,True)[1]
+print '\navg attempts to find prime', sum/(experiments*1.0)
+print 'expected number of attempts', math.log(pow(2,numbits))/2.0
