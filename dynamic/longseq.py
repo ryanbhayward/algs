d63e19faa99cc8c5bde543740521f3932b3b5429
Ryan Hayward <hayward@ualberta.ca>
2019-09-11 17:11:44 -0600

    formatted integers in lists

--- dynamic/longseq.py
+++ dynamic/longseq.py
@@ -2,7 +2,11 @@ import random  # longest increasing subsequence
 
 def genseq(n):
   S = []
-  for _ in range(n): S.append(random.randint(1,n))
+  #for _ in range(n): S.append(random.randint(1,n))
+  #return S
+  for j in range(n): 
+    S.append(j)
+  random.shuffle(S)
   return S
 
 def longest(S):
@@ -14,8 +18,14 @@ def longest(S):
     L.append(1+mymax)
   return L
 
+def pretty(S):
+  print "[",
+  for j in S:
+    print '%2d' % j, 
+  print "]"
+
 #S = [7,10,6,2,4,8,9,3,2,7,9,1]
 #S = [1,9,7,2,3,9,8,4,2,6,10,7]
 #S = [8,3,2,7,4,9,1,11,5]; print S
-S = genseq(20); print S
-L = longest(S); print L
+S = genseq(20); pretty(S); print ""
+L = longest(S); pretty(L)
