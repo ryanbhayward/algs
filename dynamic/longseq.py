fa4435d18bfd423432be09cd76a0e48cc6e9c35a
Ryan Hayward <hayward@ualberta.ca>
2019-09-17 13:22:46 -0600

    loop invariant comments

--- dynamic/longseq.py
+++ dynamic/longseq.py
@@ -10,13 +10,19 @@ def genseq(n):
   return S
 
 def longest(S):
-  L= [1] # L[j]: length longest subsequence ending at position j
+  # define f(t): length of longest increasing subsequence
+  #   of S[0 ... t] that includes S[t]
+  L= [1] # initialize L[0] to f(0)
   for k in range(1,len(S)):
     mymax = 0  # L entries positive, so this init ok
+    # for t in {0 ... k-1}, L[t] = f(t)
     for j in range(k):
       if S[j]<S[k]: mymax = max(mymax,L[j])
     L.append(1+mymax)
+  # L[k] = f(k) 
   return L
+  # for t in {0 ... n-1},  where n = len(S),
+  #   L[t] = f(t) 
 
 def pretty(S):
   print "[",
