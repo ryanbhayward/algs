dd81f09ffb67bc87a0a2e82246113a0a0bef4b07
Ryan Hayward <hayward@ualberta.ca>
2019-11-27 16:33:18 -0700

    fptas

--- hard/subsetsum.py
+++ hard/subsetsum.py
@@ -29,12 +29,38 @@ def subsetsum(S, t): #brute force subset sum
     indices = range(len(S)) # [0 1 .. n-1]
     for indexset in powerset(indices):
         s = sum(S[t] for t in indexset)
-        #if round(.999*t) <= s and round(1.001*t) >= s:
-        if s == t:
+        if round(.999*t) <= s and round(1.001*t) >= s:
+        #if s == t:
             print(indexset, s, t, end = ' ')
             print('bingo' if s==t else '')
 
-def ss2(S, t): # iterative merging
+def trim(L, ratio):
+    last = L[0]
+    Lnew = [last]
+    for j in range(1, len(L)):
+        if L[j] > last * ratio:
+            Lnew.append(L[j])
+            last = L[j]
+    return Lnew
+
+def ss2(S, t): # iterative merging, prep for fptas
+    L, Lset = [0], {0}
+    for p in S:
+        M = []
+        for x in L:
+            if x+p == t:
+                #print(L)
+                print('yes')
+                return
+            if x+p < t and x+p not in Lset:
+                M.append(x+p)
+                Lset.add(x+p)
+        L += M
+    print('no')
+
+def fptas(S, t, eps): # fptas for subset sum
+    n = len(S)
+    r = 1.0 + eps/(2.0 * n)
     L, Lset = [0], {0}
     for p in S:
         M = []
@@ -47,7 +73,10 @@ def ss2(S, t): # iterative merging
                 M.append(x+p)
                 Lset.add(x+p)
         L += M
-    print('no')
+        L.sort()
+        L = trim(L, r)
+    print('target', t, ' approx. ratio 1.0+', eps, ' approx. sum', L[len(L)-1], ' final list length', len(L), ' not exact')
+    print('')
 
 n = 12
 max_val = round(.95 * 2**n)
@@ -58,6 +87,10 @@ print('set ', S, ' target ', t)
 subsetsum(S, t)
 print('')
 ss2(S,t)
+print('')
+
+for j in [.4, .2, .1, .05, .01]:
+  fptas(S, t, j)
 
 #set  [128941, 177712, 226421, 317169, 435389, 466313, 522297, 523615, 596412, 605529, 626802, 630249, 640644, 670557, 729843, 786586, 816311, 976756, 977042, 992235]  target  5923412
 #(4, 6, 7, 8, 9, 11, 12, 18, 19) 5923412 5923412 bingo
