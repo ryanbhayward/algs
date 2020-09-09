f6031210aa046489f42f25e5d269d3c9004a5ce6
hayward <hayward@ualberta.ca>
2020-09-09 11:28:33 -0600

    collatz => python3

--- numeric/collatz.py
+++ numeric/collatz.py
@@ -1,22 +1,18 @@
+#!/usr/bin/env python3
 def collatz(n):
   while n > 1:
-    print n,
-    if n % 2 == 0:
-      n = n / 2
-    else:
-      n = n * 3 + 1
-  print n
+    print(n, end=' ')
+    if n % 2 == 0: n = n // 2
+    else: n = n*3 + 1
+  print(n)
 
 def odd_collatz(n):
   while n > 1:
-    if n % 2 == 0:
-      n = n / 2
+    if n % 2 == 0: n = n // 2
     else:
-      print n,
-      n = n * 3 + 1
-  print n
+      print (n, end=' ')
+      n = n*3 + 1
+  print(n)
 
-for j in range(10):
-  collatz(j+1)
-  #collatz(2*j+1)
-  #odd_collatz(2*j+1)
+for j in range(101):
+  collatz(j)
