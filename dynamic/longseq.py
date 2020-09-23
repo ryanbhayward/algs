0c47b22152a00bb25a90ffb6d1406a2ac9f85368
hayward <hayward@ualberta.ca>
2020-09-23 11:43:21 -0600

    tidy longseq

--- dynamic/longseq.py
+++ dynamic/longseq.py
@@ -1,15 +1,13 @@
+#!/usr/local/bin/python3
 import random  # longest increasing subsequence
 
 def pretty(S): # print vector, uniform field width
-  print "[",
-  for j in S:
-    print '%2d' % j, 
-  print "]"
+  print ('[', end=' ')
+  for j in S: print('{:01}'.format(j), end=' ')
+  print (']')
 
 def genseq(n):
   S = []
-  #for _ in range(n): S.append(random.randint(1,n))
-  #return S
   for j in range(n): 
     S.append(j)
   random.shuffle(S)
@@ -27,7 +25,6 @@ def longest(S):
       if S[j]<S[k]: sofar = max(sofar,L[j])
     # sofar = max { all j in {0 .. k-1} with S[j] < S[k]: f(j) }
     L.append(1+sofar)
-    pretty(L)
     # L[k] = f(k)
   # for n = len(S), for all t in {0 ... n-1}: L[t] = f(t) 
   return L
@@ -35,5 +32,9 @@ def longest(S):
 #S = [7,10,6,2,4,8,9,3,2,7,9,1]
 #S = [1,9,7,2,3,9,8,4,2,6,10,7]
 #S = [8,3,2,7,4,9,1,11,5]; print S
-S = genseq(20); pretty(S); print ""
+S=[  7,  5,  1,  4,  6,  2,  0,  3 ]
+#S = genseq(8); pretty(S); print ""
+#S=[  7,  5,  1,  4,  6,  2,  0,  3 ]
+pretty(S)
 L = longest(S)
+pretty(L)
