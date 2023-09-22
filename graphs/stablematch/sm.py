820bb06a097003ac80dbbcdd0b5eae4146924335
Ryan Hayward <hayward@ualberta.ca>
2023-09-22 16:24:59 -0600

    sm.py now checks whether m stable

--- graphs/stablematch/sm.py
+++ graphs/stablematch/sm.py
@@ -17,29 +17,55 @@ def show_prefs(L):
 def show_both(L,M):
   n = len(L)
   assert(n==len(M))
+  print('both preference lists')
   for j in range(n):
     print(j, ' ', L[j], ' ', M[j])
 
 def prefers(L,x,y): # in L, is x preferred to y?
   return (L.index(x) < L.index(y))
 
-def unstable_pair(A,B,e,f):# prefs, prefs, edge, edge
-  if (prefers(A[e[0]],f[1],e[1]) and prefers(B[e[1]],f[0],e[0])) or \
-     (prefers(A[f[0]],e[1],f[1]) and prefers(B[f[1]],e[0],f[0])):
-    print('unstable pair', e, f)
+def inverse_perm(P):
+  I, n = [], len(P)
+  for j in range(n):
+    I.append(P.index(j))
+  for j in range(n):
+    assert(I[P[j]]==j)
+  return I
+
+def unhappy_couple(A,B,m,minv,x,y):
+  if y == m[x]: return False
+  mx, my = m[x], minv[y] # mates of x and y
+  if (prefers(A[x],y,mx) and prefers(B[y],x,my)):
+    print(x,y,',',end=' ')
     return True
   return False
 
-def is_stable(A,B): # is (A,B) stable?
-  n = len(A)
-  for j in range(n-1):
-    for k in range(j+1,n):
-      if unstable_pair(A,B,(j,j),(k,k)):
-        return False
-  return True
+def is_stable(A,B,m): # prefs, prefs, matching m:A -> B
+  n, minv, unhappy = len(m), inverse_perm(m), False
+  print('unhappy couples:', end = ' ')
+  for j in range(n):
+    for k in range(n):
+      if unhappy_couple(A,B,m,minv,j,k):
+        unhappy = True
+  print('')
+  return not unhappy
 
-n = 3
-H = init_prefs(n)
-R = init_prefs(n)
+n = 2
+H, R = init_prefs(n), init_prefs(n)
 show_both(H,R)
-x = is_stable(H,R)
+m = list(range(n))
+
+n = 4
+H = [[2,1,3,0], [0,3,1,2], [0,1,2,3], [3,0,2,1]]
+R = [[3,2,0,1], [2,3,1,0], [2,0,1,3], [2,0,1,3]]
+m = [3,0,2,1]
+m = [2,1,0,3]
+m = [2,3,1,0]
+m = [2,3,0,1]
+
+minv = inverse_perm(m)
+print('matching and inverse', m, minv)
+if is_stable(H,R,m):
+  print('stable')
+else:
+  print('unstable')
