6d57d06fabcab1f89d94962657f126b26aab2ee4
Ryan Hayward <hayward@ualberta.ca>
2024-01-29 15:16:30 -0700

    tidied, 355 example

--- graphs/paths/astar.py
+++ graphs/paths/astar.py
@@ -1,10 +1,31 @@
-# simple astar 
+#i simple astar 
 import weight3
 def astar(G,source, target):
-  infinity = weight3.infinity(G)
+  def show_nodes(n, nodes):
+    print('\n ', end='')
+    for j in range(n):
+      print(nodes[j], end='   ')
+    print()
+
+  def show_heur(n, heur):
+    for j in range(n): 
+      print('{:3d}'.format(heuristic[nodes[j]]), end=' ')
+    print()
+
+  def show_vals(G, vals, infty):
+    for v in G:
+      print('{:3d}'.format(vals[v]) if vals[v]<infty else '---', end=' ')
+    print()
+
+  def show_done(G, done):
+    for v in G:
+      print('xxx' if v in done else '   ', end=' ')
+    print()
+    
+  infty = weight3.infinity(G)
   dist, priority, heuristic, parent, fringe, done = {}, {}, {}, {}, [], []
   for v in G:
-    dist[v], priority[v], parent[v] = infinity, infinity, -1
+    dist[v], priority[v], parent[v] = infty, infty, -1
   dist[source], priority[source], parent[source] = 0, 0, source
 
   #nodes, hvals = ['A','B','C','Z'], [0, 20, 22, 0]
@@ -13,18 +34,23 @@ def astar(G,source, target):
   #              [  0, 26, 24, 22, 18,  7,  2, 0 ]
   #              [  0, 26, 25, 22, 17,  7,  2, 0 ]
   #              [  0, 26, 24, 22,  2,  7, 10, 0 ]
-  nodes, hvals = ['A','B','C','D','E','F','G','Z'],\
-                 [  0, 26, 25, 22,  2,  7, 10, 0 ]
-  assert len(nodes)==len(hvals)
-  for j in range(len(nodes)): heuristic[nodes[j]] = hvals[j]
-  for j in range(len(nodes)): print(nodes[j], heuristic[nodes[j]])
+  #nodes, hvals = ['A','B','C','D','E','F','G','Z'],\
+  #               [  0, 26, 25, 22,  2,  7, 10, 0 ]
+  nodes, hvals = ['A','B','C','D','F','L','M','P','Q','R','S','T','Z'],\
+                 [366, 0, 160,242,176,244,241,100,380,193,253,329,374]
+  n = len(nodes)
+  assert n==len(hvals)
+  for j in range(n): 
+    heuristic[nodes[j]] = hvals[j]
+  show_nodes(n, nodes)
+  show_heur(n, heuristic)
 
   msg = '*'
   fringe.append(source)
   while len(fringe) > 0:
     current = fringe.pop(weight3.indexOfMin(fringe, priority))
     done.append(current)
-    print('current', current, 'priority', priority[current])
+    print('\n', current, 'done: dist-from-source', dist[current], 'est-total', priority[current], '\n')
     msg = msg + current + '*' + str(priority[current]) + '*'
     if current == target: break
     for (v, wuv) in G[current]:
@@ -35,11 +61,16 @@ def astar(G,source, target):
           parent[v] = current
           priority[v] = new_v_dist + heuristic[v]
           if v not in fringe: fringe.append(v)
-
+    show_nodes(n, nodes)
+    show_heur(n, heuristic)
+    show_vals(G, dist, infty)
+    show_done(G, done)
+    show_vals(G, priority, infty)
   print(msg)
   print('')
 
 #G = weight3.G8
-G = weight3.G9
-print(G)
-astar(G,'A','Z')
+G = weight3.G355
+#print(G)
+astar(G,'A','B')
+# 
