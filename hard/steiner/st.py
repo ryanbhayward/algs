3c1898af9dfb519651c3cab432854b4a7e553128
Ryan Hayward <hayward@ualberta.ca>
2024-07-08 14:02:30 -0600

    ok up to 3 terminals

--- hard/steiner/st.py
+++ hard/steiner/st.py
@@ -1,6 +1,8 @@
 # min weight rectilinear steiner tree    rbh 2024
 
-# started :)
+# started
+# correct if at most 3 terminals :)
+# todo: what if 4 or more terminals?
 
 from operator import itemgetter as ig
 
@@ -13,6 +15,20 @@ def minmax(T): #min x-coord, min y, max x, max y
 
 def report(T):
   print('\n', T, '\n', minmax(T), sep='')
+  print(rst(T))
+
+def rst(T): # min wt rect steinter tree
+  n = len(T)
+  if n == 1:
+    return 0
+  minx, miny, maxx, maxy = minmax(T)
+  thin = min(miny - minx, maxy - maxx)
+  thick = max(miny - minx, maxy - maxx)
+  if n == 2 or n ==3:
+    return thin + thick
+  if n == 4:
+    return thin + thin + thick
+  else: assert False
 
 T = ((0,5), (2,1), (4,7), (6,3))
 
