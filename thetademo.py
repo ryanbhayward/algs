61f74d6a273bb582763e03226c2bd1ca9501eaa7
Ryan Hayward <hayward@ualberta.ca>
2024-09-11 13:03:08 -0600

    tweaked thetademo

--- thetademo.py
+++ thetademo.py
@@ -1,13 +1,14 @@
 from math import log
 '''
 an example of a function that is in Theta(n^{2.5}),
-but the ratio of f(2n)/f(n), expected
-to be about (2n)^{2.5} / n^{2.5} or roughly 5.6,
+but the ratio of f(2n)/f(n), 
+which will eventually converge to (2n)^{2.5} / n^{2.5} or roughly 5.6,
 is about ten times that when n is 3700000...
 
 why? because at this value of n, 
 the slower growing log(n)^{90} dominates ...
-once n gets large enough, the faster growing n^{2.5} dominates :)
+once n gets large enough, 
+the faster growing n^{2.5} dominates :)
 '''
 
 def f(n):
