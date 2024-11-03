99f14ea81dcaf7c1f398e1e37faa6ea0d918ea13
Ryan Hayward <hayward@ualberta.ca>
2024-11-03 15:09:24 -0700

    why does scc2 not always work?

--- graphs/decomp/scc24.py
+++ graphs/decomp/scc24.py
@@ -1,21 +1,32 @@
 # revised 2024 rbh, dfs now returns list of components
 #   so scc is just components of dfs of transpose
 
-def postorder(G, v, seen, L):
+def dfs_postorder(G, v, seen, L):
    seen[v] = True
    for nbr in G[v]:
      if not seen[nbr]:
-       postorder(G, nbr, seen, L)
+       dfs_postorder(G, nbr, seen, L)
    L.append(v)
 
-def dfs(D):
+def node_list(D):
+  L = [v for v in D]
+  return L
+
+def get_nodes(C):
+   L = []
+   for component in C:
+     for node in component:
+       L.append(node)
+   return L
+
+def dfs(D, nodes):
   seen = {v:False for v in D}
   C = [] # list of components
   # each component is a list of nodes, in postorder
-  for v in D:
+  for v in nodes:
     if not seen[v]:
       L = []
-      postorder(D, v, seen, L)
+      dfs_postorder(D, v, seen, L)
       C.append(L)
   return C
 
@@ -26,27 +37,35 @@ def transpose(D):
       T[w].append(v)
   return T
 
-def show(D):
+def show_graph(D):
   for v in D:
     print(v, ': ', end='')
     for w in D[v]:
       print(w, end=' ')
     print()
 
-def scc(D):
-   print('\ndigraph')
-   show(D)
-   T = transpose(D)
-   print('\ntranspose')
-   show(T)
-   POT = dfs(T)
+def show(sccs):
    print('\nstrongly connected components')
-   for component in POT:
+   for component in sccs: 
      for node in component:
         print(node, end='')
      print()
 
-from graphs_input import D, D0, D2, D3, D4, D5, G
-digraphs = [D, D0, D2, D3, D4, D5, G]
-for d in digraphs:
+def scc(D):
+   T = transpose(D)
+   PO = get_nodes(dfs(T, node_list(T)))
+   PO.reverse()
+   sccs = dfs(D, PO)
+   show(sccs)
+
+def scc2(D): # this doesn't always work: why?
+   PO = get_nodes(dfs(D, node_list(D)))
+   sccs = dfs(D, PO)
+   show(sccs)
+
+from graphs_input import D, D0, D1, D2, D3, D4, D5, D6, G
+for d in [D, D0, D1, D2, D3, D4, D5, D6, G]:
+  print('\nnext digraph')
+  show_graph(d)
   scc(d)
+  scc2(d)
