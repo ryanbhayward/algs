6e370ac7e5fc4591137189b971b72d605fb77120
Ryan Hayward <hayward@ualberta.ca>
2024-11-03 12:58:20 -0700

    revised scc

---
+++ graphs/decomp/scc24.py
@@ -0,0 +1,51 @@
+# revised 2024
+
+def postorder(G, v, seen, L):
+   seen[v] = True
+   for nbr in G[v]:
+     if not seen[nbr]:
+       postorder(G, nbr, seen, L)
+   L.append(v)
+
+def dfs(D):
+  seen = {v:False for v in D}
+  C = [] # list of components
+  # each component is a list of nodes, in postorder
+  for v in D:
+    if not seen[v]:
+      L = []
+      postorder(D, v, seen, L)
+      C.append(L)
+  return C
+
+def transpose(D):
+  T = {v:[] for v in D}
+  for v in D: 
+    for w in D[v]: 
+      T[w].append(v)
+  return T
+
+def show(D):
+  for v in D:
+    print(v, ': ', end='')
+    for w in D[v]:
+      print(w, end=' ')
+    print()
+
+def scc(D):
+   print('\ndigraph')
+   show(D)
+   T = transpose(D)
+   print('\ntranspose')
+   show(T)
+   POT = dfs(T)
+   print('\nstrongly connected components')
+   for component in POT:
+     for node in component:
+        print(node, end='')
+     print()
+
+from graphs_input import D, D0, D2, D3, D4, D5, G
+digraphs = [D, D0, D2, D3, D4, D5, G]
+for d in digraphs:
+  scc(d)
